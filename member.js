f4bf68c866186c060ffddd228aaa444c3b4b02a4
Ankita Dharne <46591590+ankita-dharne@users.noreply.github.com>
2023-08-08 08:42:17 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,15 @@
+function skillsMember() {
+  var member = document.getElementById("member");
+  var skills = document.getElementById("skills");
+  var experience = document.getElementById("experience");
+  var education = document.getElementById("education");
+  var projects = document.getElementById("projects");
+  var contact = document.getElementById("contact");
+
+  member.style.display = "block";
+  skills.style.display = "none";
+  experience.style.display = "none";
+  education.style.display = "none";
+  projects.style.display = "none";
+  contact.style.display = "none";
+}
